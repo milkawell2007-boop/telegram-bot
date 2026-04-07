5bdc8d3948ce9344759407a6286f9617eb741471
milkawell2007-boop <milkawell2007@gmail.com>
2026-04-08 00:23:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import os
 import telebot
 import time
 import threading
@@ -43,5 +44,5 @@ def home():
 if __name__ == "__main__":
     t = threading.Thread(target=run_bot)
     t.start()
-
-    app.run(host="0.0.0.0", port=10000)
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
