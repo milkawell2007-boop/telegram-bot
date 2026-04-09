561e051ddf4a613c448a89212d817c297c56c703
milkawell2007-boop <milkawell2007@gmail.com>
2026-04-09 14:09:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,8 @@
 import os
 import telebot
-import time
-import threading
-from flask import Flask
+from flask import Flask, request
 
-TOKEN = '8673666660:AAHoGsYm0R8XENa4Itm9kAroY7jyBjW2AcE'
+TOKEN = '8673666660:AAH80TlEhV-gcVZ8jOR_BLUTAepwxd3J9qo'
 
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
@@ -19,38 +17,28 @@ users = [
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-    print("ПРИШЛО СООБЩЕНИЕ:", message.text)
+    print("ПРИШЛО:", message.text)
 
     if message.text and "созвать конченных" in message.text.lower():
-        try:
-            bot.send_message(message.chat.id, "начинаю созыв конченных")
-            bot.send_message(message.chat.id, " ".join(users))
-        except Exception as e:
-            print(f"Ошибка: {e}")
+        bot.send_message(message.chat.id, "начинаю созыв конченных")
+        bot.send_message(message.chat.id, " ".join(users))
 
-def run_bot():
-    print("БОТ ЗАПУЩЕН")
-    while True:
-        try:
-            print("Polling start…")
-            bot.polling(none_stop=True, interval=1, timeout=20)
-        except Exception as e:
-            print(f"Ошибка бота: {e}")
-            time.sleep(5)
-            
-    while True:
-        try:
-            bot.polling(none_stop=True, interval=1, timeout=20)
-        except Exception as e:
-            print(f"Ошибка бота: {e}")
-            time.sleep(5)
+@app.route(f"/{TOKEN}", methods=["POST"])
+def webhook():
+    json_str = request.get_data().decode("UTF-8")
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return "OK", 200
 
-@app.route('/')
+@app.route("/")
 def home():
     return "Bot is alive"
 
 if __name__ == "__main__":
-    threading.Thread(target=run_bot, daemon=True).start()
+    WEBHOOK_URL = f"https://telegram-bot-jcef.onrender.com/{TOKEN}"
+
+    bot.remove_webhook()
+    bot.set_webhook(url=WEBHOOK_URL)
 
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
+    app.run(host="0.0.0.0", port=port)
